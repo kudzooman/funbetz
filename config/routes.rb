6876b35c0dbad47859d01a8b38df7e551ddb4bc7
Chris Elwood <kudzooman@gmail.com>
2015-08-02 15:39:08 -0400

    created main page layout and card controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  get 'welcome/about'
+  resources :cards
+
+  get 'home' => 'welcome#index'
+
+  get 'about' => 'welcome#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
